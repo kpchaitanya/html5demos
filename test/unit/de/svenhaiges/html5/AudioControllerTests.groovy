c144a6ec92e16046d41594d93104a052d9a9cded
sven.haiges <sven.haiges@16a69898-1658-ad0c-3750-fab67d35ce44>
2010-03-16 03:58:38 +0000

    added structure

---
+++ test/unit/de/svenhaiges/html5/AudioControllerTests.groovy
@@ -0,0 +1,17 @@
+package de.svenhaiges.html5
+
+import grails.test.*
+
+class AudioControllerTests extends ControllerUnitTestCase {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testSomething() {
+
+    }
+}
---
+++ test/unit/de/svenhaiges/html5/CanvasControllerTests.groovy
@@ -0,0 +1,17 @@
+package de.svenhaiges.html5
+
+import grails.test.*
+
+class CanvasControllerTests extends ControllerUnitTestCase {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testSomething() {
+
+    }
+}
