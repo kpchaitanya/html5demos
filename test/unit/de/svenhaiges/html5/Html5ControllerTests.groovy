68512f9ad0c8fe2fe6687313bed63b491caf86a1
sven.haiges <sven.haiges@16a69898-1658-ad0c-3750-fab67d35ce44>
2010-03-18 05:05:46 +0000

    added a few images, iwebkit v1

---
+++ test/unit/de/svenhaiges/html5/Html5ControllerTests.groovy
@@ -0,0 +1,17 @@
+package de.svenhaiges.html5
+
+import grails.test.*
+
+class Html5ControllerTests extends ControllerUnitTestCase {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testSomething() {
+
+    }
+}
